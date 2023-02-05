c75904c6dd2239734f29bd252b5e71fd70a25959
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-02-05 14:21:17 +0330

    add html & css & reset file

--- q1/script.js
+++
@@ -1,8 +0,0 @@
-const counter = counterMaker();
-console.log(counter()); // 0
-console.log(counter()); // 0
-console.log(counter(1)); // 1
-console.log(counter()); // 1
-console.log(counter(3)); // 4
-console.log(counter(7)); // 11
-console.log(counter(-5)); // 6
