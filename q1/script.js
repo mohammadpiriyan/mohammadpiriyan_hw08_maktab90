a08c7ecacaed962603e50d77f0e96d15fb69418c
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-02-08 20:34:11 +0330

    complete task

--- q1/script.js
+++ q1/script.js
@@ -1,4 +1,14 @@
-const callback = function (elem) {
-  elem.style.backgroundColor = "red";
+// const callback = function (elem) {
+//   elem.style.backgroundColor = "red";
+// };
+// elemCreator("div", callback);
+const elemCreator = function (element, cb) {
+  const newElem = document.createElement(element);
+  cb(newElem);
+  document.body.append(newElem);
 };
-elemCreator("div", callback);
+elemCreator("div", (elem) => {
+  elem.style.backgroundColor = "blue";
+  elem.style.width = "250px";
+  elem.style.height = "250px";
+});
