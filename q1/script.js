8b31cbe318e0a6b6e7e085b54517bad9f4ad2600
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-02-08 20:35:42 +0330

    complete task

--- q1/script.js
+++ q1/script.js
@@ -6,3 +6,11 @@ console.log(counter()); // 1
 console.log(counter(3)); // 4
 console.log(counter(7)); // 11
 console.log(counter(-5)); // 6
+
+const counterMaker = function () {
+  let counter = 0;
+  return function (number = 0) {
+    counter = counter + number;
+    return counter;
+  };
+};
